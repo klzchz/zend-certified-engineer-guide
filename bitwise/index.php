d1534c3889a34115a53fe35c354bca16eaeec81e
Lucas Andrade <lucascorporation@live.com>
2023-04-18 19:29:06 -0300

    bitwise

---
+++ bitwise/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+                    ##Bitwise Operators##
+##########################################################################################################################
+##  Example	           Name	        Result                                                                               ##      
+##  $a & $b	           And  	     Bits that are set in both $a and $b are set.                                        ##
+##  $a | $b	           Or            (inclusive or)	Bits that are set in either $a or $b are set.                        ##
+##  $a ^ $b	           Xor           (exclusive or)	Bits that are set in $a or $b but not both are set.                  ##
+##  ~ $a	           Not	          Bits that are set in $a are not set, and vice versa.                               ##
+##  $a << $b	      Shift left   	  Shift the bits of $a $b steps to the left (each step means "multiply by two")      ##
+##  $a >> $b	      Shift right	  Shift the bits of $a $b steps to the right (each step means "divide by two")       ## 
+##########################################################################################################################
+
+*/
